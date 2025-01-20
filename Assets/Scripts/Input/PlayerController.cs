1294c2056d71637abddc637b41c360b3ca7ac685
Erik <vyserik@hotmail.com>
2025-01-19 21:19:04 -0500

    Ajout de la rotation du joueur et de la bar d'impact Good/Bad

--- Assets/Scripts/Input/PlayerController.cs
+++ Assets/Scripts/Input/PlayerController.cs
@@ -37,5 +37,11 @@ public class PlayerController : MonoBehaviour
     {
         movement.Set(playerControls.Player.Movement.ReadValue<Vector2>().x, playerControls.Player.Movement.ReadValue<Vector2>().y);
         rb.velocity = movement * speed;
+
+        if(movement != Vector2.zero)
+        {
+            float angle = Mathf.Atan2 (movement.y,movement.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0,0,angle - 90f);
+        }
     }
 }
