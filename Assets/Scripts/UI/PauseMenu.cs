aa9ee0db2d7b306b281b512893aa11a3f028ed44
Jacob Marquis <infinity280204@gmail.com>
2025-01-27 19:40:40 -0500

    Ajout du menu pause et de son code.

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,45 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public InputActionReference pauseGame;
+    [SerializeField] GameObject pauseMenu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pauseMenu.SetActive(false);
     }
-
     // Update is called once per frame
     void Update()
     {
         
     }
-
-    void OnEnable()
+    //Est appeller quand le boutton pause est appuyer
+    public void Pause()
     {
-        pauseGame.action.started += OpenPauseMenu;
-        Debug.Log("Done");
+        pauseMenu.SetActive(true);
+        Time.timeScale=0;
     }
 
-    void OnDisable()
+    public void Resume()
     {
-        pauseGame.action.started -= OpenPauseMenu;
+        pauseMenu.SetActive(false);
+        Time.timeScale=1;
     }
-
+    //
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale=1;
+    }
     
-    //Est appeller quand la touche ESC est press ( en temps normal...)
-    private void OpenPauseMenu(InputAction.CallbackContext context)
-    {
-        Debug.Log("TABARNAK");
-    }
-
-
-
-
 
 
 }
